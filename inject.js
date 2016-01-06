6f807d6e384c5e99c650f03763d08b07e24c06a8
guy mograbi <guym@gigaspaces.com>
2016-01-06 13:15:24 +0200

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+
+var path = require('path');
+
+var filename = process.argv.length > 2 ? process.argv[2] : process.argv[1];
+filename = path.resolve(filename);
+
+
+if ( filename === path.resolve(__filename) ){
+	console.log('missing input file argument');
+	process.exit(1);
+}
+
+var data = require( filename );
+
+
+
+function replace_data( data ){
+	Object.keys(data).forEach(function(key){
+		value = data[key];
+		if ( typeof(value) === 'object' ) {
+			replace_data(value);
+		}else{
+			if ( process.env.hasOwnProperty(value)){
+				data[key] = process.env[value];
+			}
+		}
+	});
+	return data;
+}
+
+require('fs').writeFile( filename , JSON.stringify(replace_data(data), {} , 4 ) );
